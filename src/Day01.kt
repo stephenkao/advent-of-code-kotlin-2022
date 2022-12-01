ed2e4c76d19a2b98fd89b9741dee74a883fdc3cc
Stephen Kao <stephenkao@navapbc.com>
2022-12-01 17:45:59 -0500

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var max = 0
+
+        for (line in input) {
+            if (line == "") {
+                if (max < sum) {
+                    max = sum;
+                }
+
+                sum = 0;
+            } else {
+                sum += line.toInt()
+            }
+        }
+
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sumList = mutableListOf<Int>()
+        var currentSum = 0
+        for ((index, line) in input.withIndex()) {
+            if (line == "") {
+                sumList.add(currentSum)
+                currentSum = 0
+            } else {
+                currentSum += line.toInt()
+            }
+
+            if (index == input.lastIndex) {
+                sumList.add(currentSum)
+                currentSum = 0
+            }
+        }
+
+        return sumList.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
